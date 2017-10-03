1e3175eb5703981bdfd4f801633fdda0230ba713
Theo PACALY <tpacaly@e2r2p22.42.fr>
2017-10-03 16:58:58 +0200

    g

--- build.sh
+++
@@ -1,5 +0,0 @@
-#cd libft && make fclean && make && cd ..
-#cd minilibx_macos0 && make re && cd ..
-#gcc -Wall -Werror -Wextra -framework OpenGL -framework AppKit src/fdf.c libft/libft.a minilibx_macos0/libmlx.a -o fdf
-gcc -Wall -Werror -Wextra -framework OpenGL -framework AppKit src/print.c src/parser.c src/math.c src/fdf.c libft/libft.a minilibx_macos/libmlx.a -o fdf
-./fdf maps/good/42.fdf
